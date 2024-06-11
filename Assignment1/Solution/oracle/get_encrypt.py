07f21c275243964924c665491aa6d38b28a4ce92
Anjali Singh <67422710+AnjaliSingh181930@users.noreply.github.com>
2024-06-11 14:27:21 +0530

    Create get_encrypt.py

---
+++ Assignment1/Solution/oracle/get_encrypt.py
@@ -0,0 +1,11 @@
+# Write your script here
+from random import randbytes
+
+class Get_Enc(object): # Do not change this
+    def get_entry(self):
+        entry_number="2023JCS2565"  #Enter you Entry Number here in the this format - 2022JCS2669 (All Capital)
+        return entry_number
+
+    def enc(self): #Do not change this
+        plaintext= ["First Plaintextt", "Second Plaintextt", "Third Plaintextt"]  #Enter you plaintext here.
+        return plaintext
